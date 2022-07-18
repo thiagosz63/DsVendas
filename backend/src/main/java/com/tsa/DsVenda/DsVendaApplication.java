cc27a1f7d81fbc3c47855fdbf4022d8e1d4a56ac
thiagosz63 <thiagosz63@gmail.com>
2022-07-18 20:31:23 -0300

    Project Created

---
+++ backend/src/main/java/com/tsa/DsVenda/DsVendaApplication.java
@@ -0,0 +1,13 @@
+package com.tsa.DsVenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendaApplication.class, args);
+	}
+
+}
