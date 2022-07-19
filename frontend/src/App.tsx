caa486026cfed30a859b1bffb0cc65589f85252d
thiagosz63 <thiagosz63@gmail.com>
2022-07-18 21:20:41 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import DataTable from './components/DataTable';
 
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
-
-    </div>
+    <>
+      <NavBar />
+      <div className='container'>
+        <h1 className='text-primary'>Ola mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
