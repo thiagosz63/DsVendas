57048faf26a59c79f256031e2d094c12faf291bf
thiagosz63 <thiagosz63@gmail.com>
2022-07-25 20:38:01 -0300

    Home, navigation

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 
 export default function Routes() {
     return (
         <BrowserRouter>
+            <NavBar />
             <Switch>
                 <Route path="/" element={<Home />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Switch>
+            <Footer />
         </BrowserRouter>
 
     )
